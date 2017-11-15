38b6adbcb5a7235ce738628c7e81056c07d1d053
Hannah Kelly <hk2781@nyu.edu>
2017-11-15 07:04:11 -0500

    added more test cases

--- src/test/java/edu/nyu/oop/ClassTreeVisitorTest.java
+++ src/test/java/edu/nyu/oop/ClassTreeVisitorTest.java
@@ -3,6 +3,8 @@ import org.junit.*;
 import org.slf4j.Logger;
 import xtc.tree.Node;
 import xtc.tree.GNode;
+
+import java.lang.reflect.Method;
 import java.util.Map;
 import java.util.List;
 import java.util.ArrayList;
@@ -33,12 +35,59 @@ public class ClassTreeVisitorTest {
         for(MethodSignature method : methods){
             String method_name = method.getMethodName();
             allMethods.add(method_name);
-            logger.debug(method_name);
         }
         assertTrue(allMethods.contains("toString"));
     }
 
+    public void test003(){
+        ClassTreeVisitor visitor = new ClassTreeVisitor();
+        List<Node> input_ast = new ArrayList<Node>();
+        GNode node = (GNode) XtcTestUtils.loadTestFile("src/test/java/inputs/test003/Test003.java");
+        input_ast.add(node);
+        Map<String,ClassSignature> tree_map = visitor.getClassTree(input_ast);
 
+        current_class = tree_map.get("A");
+        List<ConstructorSignature> constructors = current_class.getConstructorList();
+        ArrayList<String> allConstructors = new ArrayList<String>();
+        for(ConstructorSignature constructor : constructors){
+            String constructor_name = constructor.getName();
+            allConstructors.add(constructor_name);
+        }
+        assertTrue(allConstructors.contains("A"));
 
+        List<MethodSignature> methods = current_class.getMethodList();
+        ArrayList<String> allMethods = new ArrayList<String>();
+        for(MethodSignature method : methods){
+            String method_name = method.getMethodName();
+            allMethods.add(method_name);
+        }
+        assertTrue(allMethods.contains("getFld"));
+
+    }
+
+    public void test012(){
+        ClassTreeVisitor visitor = new ClassTreeVisitor();
+        List<Node> input_ast = new ArrayList<Node>();
+        GNode node = (GNode) XtcTestUtils.loadTestFile("src/test/java/inputs/test012/Test012.java");
+        input_ast.add(node);
+        Map<String,ClassSignature> tree_map = visitor.getClassTree(input_ast);
+
+        current_class = tree_map.get("C");
+        List<ConstructorSignature> constructors = current_class.getConstructorList();
+        ArrayList<String> allConstructors = new ArrayList<String>();
+        for(ConstructorSignature constructor : constructors){
+            String constructor_name = constructor.getName();
+            allConstructors.add(constructor_name);
+        }
+        assertTrue(allConstructors.contains("c"));
+
+        List<MethodSignature> methods = current_class.getMethodList();
+        ArrayList<String> allMethods = new ArrayList<String>();
+        for(MethodSignature method : methods){
+            String method_name = method.getMethodName();
+            allMethods.add(method_name);
+        }
+        assertTrue(allMethods.contains("myToString"));
+    }
 
 }
